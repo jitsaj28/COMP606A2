aa2c0f40d313b596b18a452d2ccf925b84d53aee
sudharsan13479 <52937190+sudharsan13479@users.noreply.github.com>
2019-11-03 20:36:02 +1300

    header file to call my booststrap and css

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Trade Title</title>
+  <link rel="icon" href="Images/Button32x32.png" type="image/x-icon">
+  <meta name="theme-color" content="#555555" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="Css/bootstrap.min.css">
+  <script src="Script/bootstrap.min.js"></script>
+  <script src="Script/Jquery3.3.1.js"></script>
+  <script src="Script/popper.min.js"></script>
+</head>
+<body>
+
+<?php
+require_once 'lib/inc_classes.php';
+?>
+
