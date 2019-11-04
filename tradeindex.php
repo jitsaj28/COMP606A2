9ae847e30ac1e9e413844e57fd8de2ee6cdc85d6
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-11-04 15:38:12 +1300

    Trading view
    Tradeing  list view

--- tradeindex.php
+++ tradeindex.php
@@ -1,41 +1,52 @@
 <?php
  include 'header.php' ; 
+ include 'navbartrademan.php' ; 
 
  
-    $ClassObject = new person();
+    $ClassObject = new Customer();
  ?> 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark  justify-content-center">
- 
-            <a class="navbar-brand" href="#">TradeMan</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-     
-         
-    </ul>
-  </div>  
-</nav>
+<script>
+$(document).ready(function() 
+{
+			var ms = getQueryString("msg");
+			if(ms == "s")
+			{
+			$('#btnsuccess').click();
+			}
+			
+});
+</script>
+<div class="row container-fluid" style="background-color:#ffffb9;height:100vh;width:99.8vw;padding-top:10px;">
 <br>
  <?php  
+ if (is_a($ClassObject, 'DBConnection'))
+	{
         $post_data =  $ClassObject->get_jobdetails('trade');  
+		}
       foreach($post_data as $post)  
          {  
          ?>  
-            <div class="container" >     
-          <h3> <?php echo $post["jtitle"]; ?> </h3>
+		 <div class="col-md-3 container-fluid" style="margin-left:50px;margin-right:50px;border:1px solid black;height:270px;border-radius:3%;"> 
+		<div class="row">
+		<div class="col-md-12">
+              <h3> <?php echo $post["jtitle"]; ?> </h3>
               <p>   <?php echo $post["jdesc"]; ?> </p>
-			  <div>
-			  <span style="float:left;">Cost : $<?php echo $post["jcost"]; ?></span><br>
-			   <span style="float:left;">Upload date :<?php echo $post["jadate"]; ?></span><br>
-			  <span style="float:right;"> Deadline date :<?php echo $post["jddate"]; ?></span><br>
-			  <span style="float:right;"><a href="ApplyQ.php?id=<?php echo $post["id"]; ?>" ><button class="btn btn-success" >Apply quote</button></a></span>			  
+			</div>
+			<div class="col-md-6">
+			  <p >Cost :<br> $<?php echo $post["jcost"]; ?></p>
+			   <p >Upload date :<br><?php echo $post["jadate"]; ?></p>
+			   </div>
+			<div class="col-md-6">
+			  <p > Deadline date :<br><?php echo $post["jddate"]; ?></p>
 			  </div>
+			  <div class="col-md-12">
+			  <a href="ApplyQ.php?id=<?php echo $post["id"]; ?>" ><button class="btn btn-success btn-block" >Apply quote</button></a>
+			  </div>
+         </div>
          </div>
        <?php  
     }  
 ?>  
-<div class="container">
+
 </div>
 <?php include 'footer.php'; ?>
