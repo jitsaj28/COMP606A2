2d0b915ceb969d177a9637e88146c11406ab395b
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-11-04 17:06:54 +1300

    navigation bar
    navigation bar for the customerview

---
+++ NavBarCustomer.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark  justify-content-center">
+ 
+            <a class="navbar-brand" href="CustomerView.php">Trade</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">      
+       <li class="nav-item">
+        <a class="nav-link" href="insert.php">Add Job</a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="CustomerViewTrade.php">View Quotes</a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="ConfiredJob.php">Confired</a>
+      </li>
+	   <li class="nav-item" style="float:left;">
+        <a class="nav-link" href="Logout.php">Logout</a>
+      </li>
+    </ul>
+  </div>  
+</nav>
