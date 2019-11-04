1ab56733c076f7a0927871ddc5a336a8e2a3819c
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-11-04 21:27:12 +1300

    Customer Login
    Customer Login

--- index.php
+++ index.php
@@ -38,9 +38,9 @@ $(document).ready(function()
         
         ?>
 		
-<div class="row container-fluid" style="background-image:url('Images/1.jpg'); height:100vh;">
+<div class="row container-fluid" style="background-image:url('Images/13.png'); height:100vh;">
 <div class = "col-md-4" style="margin-left:35%;margin-top:10%;">
-      <div class="container-fluid" style="border:1px solid #fff0ff;padding:5%;border-radius:5%;">
+      <div class="container-fluid" style="border:1px solid white;padding:5%;border-radius:5%;">
 	  
 	  
 		<h3>Login Customer</h3>
