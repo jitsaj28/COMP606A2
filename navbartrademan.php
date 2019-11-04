800a1da6be5355781b971aaee743c2aa8a20c201
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-11-04 17:07:35 +1300

    navigation bar
    navigation bar for the tradesman view

---
+++ navbartrademan.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark  justify-content-center">
+ 
+            <a class="navbar-brand" href="tradeindex.php">TradeMan</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="TradeApplied.php">View Applied Job</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="TradeConfiredJob.php">Confirmed Job</a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+         
+    </ul>
+  </div>  
+</nav>
