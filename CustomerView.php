1945ab786282e409a8ab68e6c867a0e621f02ea3
jitsaj28 <52937081+jitsaj28@users.noreply.github.com>
2019-11-04 15:40:16 +1300

    job
    add new jobs

--- CustomerView.php
+++ CustomerView.php
@@ -9,6 +9,20 @@
  ?> 
 
 <br>
+
+<script>
+          
+$(document).ready(function() 
+{
+			var ms = getQueryString("msg");
+			if(ms == "s")
+			{
+			$('#btnsuccess').click();
+			}
+			
+});
+	</script>
+<div class="row container-fluid" style="background-color:#ffffb9;height:100vh;width:99.8vw;margin-top:-25px;padding-top:10px;">
  <?php  if (is_a($ClassObject, 'DBConnection'))
 	{
         $post_data =  $ClassObject->get_jobdetails('customer');  
@@ -18,26 +32,38 @@
       foreach($post_data as $post)  
          {  
          ?>  
-            <div class="container" >     
-          <h3> <?php echo $post["jtitle"]; ?> </h3>
+            <div class="col-md-3 container-fluid" style="margin-left:50px;margin-right:50px;border:1px solid black;height:200px;border-radius:3%;"> 
+		<div class="row">			
+         <div class="col-md-12">
+		 <h3> <?php echo $post["jtitle"]; ?> </h3>
               <p>   <?php echo $post["jdesc"]; ?> </p>
-			  <div>
+			
+			 </div>
 			 
-			  <span style="float:left;"> Cost : $<?php echo $post["jcost"]; ?></span><br>
-			  <span style="float:left;"> Status :<?php  if($post["tradeid"] == 0){echo "On" ;}else { echo "Off";} ?></span>
-			  <span style="float:right;">Upload date :<?php echo $post["jadate"]; ?></span><br>
-			  <span style="float:right;"> Deadline date :<?php echo $post["jddate"]; ?></span>
+<div class="col-md-6">			 
+			 <p>Upload date  <br><?php echo $post["jadate"]; ?></p>
+			  <p> Cost  : $<?php echo $post["jcost"]; ?></p>
+			  
 			  
 			  </div>
+			  <div class="col-md-6">
+			  <p> Deadline date <br><?php echo $post["jddate"]; ?></p>
+			  </div>
          </div>
+         </div>
+		 
+		 
        <?php  
-    }  
+    }
+	
 	}
+	
 	else{
-	echo "There is no record found";
+	?>
+	<div>There no record</div>
+	<?php 
 	}
 ?>  
-<div class="container">
 </div>
 <?php 
 }
